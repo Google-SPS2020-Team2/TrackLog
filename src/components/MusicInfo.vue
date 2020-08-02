c7f9a0f8c8377c3304088fe4657aaa91cd2fdb8e
Tianyun Zhang <doowzs@qq.com>
2020-08-02 16:23:32 +0800

    add simple layout for music list

--- src/components/MusicInfo.vue
+++ src/components/MusicInfo.vue
@@ -1,10 +1,29 @@
 <template>
-  <p>Music info page</p>
+  <div v-bind:id="'music-' + music.id " style="margin-bottom: 1rem;">
+    <md-card>
+      <md-card-header>
+        <span class="md-title">{{ music.music_name }}</span>
+      </md-card-header>
+      <md-card-content>
+        <p>Artist ID: {{ music.artist_id }}</p>
+        <p>Difficulty: {{ music.difficulty }}</p>
+      </md-card-content>
+      <md-card-actions>
+        <button class="md-button">
+          <md-icon>favorite</md-icon> &nbsp; <span>Favor</span>
+        </button>
+        <button class="md-button">
+          <md-icon>play_circle_filled</md-icon> &nbsp; <span>Play</span>
+        </button>
+      </md-card-actions>
+    </md-card>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "MusicInfo"
+  name: "MusicInfo",
+  props: ['music']
 }
 </script>
 
--- src/components/MusicList.vue
+++ src/components/MusicList.vue
@@ -1,15 +1,48 @@
 <template>
-  <p @click="fetchMusic">Music list page</p>
+  <div v-if="loading">
+    <md-progress-spinner md-mode="indeterminate"></md-progress-spinner>
+    <p>{{ message }}</p>
+  </div>
+  <div v-else-if="musics.length" id="music-list">
+    <music-info v-for="music in musics"
+                v-bind:key="music.id"
+                v-bind:music="music"
+    ></music-info>
+  </div>
+  <div v-else id="no-music">
+    <p>There is no music to show.</p>
+  </div>
 </template>
 
 <script>
+import MusicInfo from "@/components/MusicInfo";
+
 export default {
   name: "MusicList",
+  components: {
+    'music-info': MusicInfo
+  },
+  data: function () {
+    return {
+      loading: true,
+      message: 'Loading...',
+      musics: []
+    }
+  },
+  created: function () {
+    this.getMusics();
+  },
   methods: {
-    fetchMusic() {
+    getMusics() {
       this.$http.get('/show')
-        .then(res => console.log(res.data))
-        .catch(err => console.error(err));
+          .then(res => {
+            this.musics = res.data;
+            this.loading = false;
+          })
+          .catch(err => {
+            console.error(err);
+            this.message = err;
+          });
     }
   }
 }
