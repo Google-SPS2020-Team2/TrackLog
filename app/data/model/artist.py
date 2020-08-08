136dce2f4bbc55b6d58969d5420f00e54f4c17cb
ShenRuobing <1700012796@pku.edu.cn>
2020-08-08 18:08:41 +0800

    Add  Add an API to list played music of a user,Get artist info from artist id

--- app/data/model/artist.py
+++ app/data/model/artist.py
@@ -1,3 +1,22 @@
+@bp.route("/get_artist_info",methods=("GET", "POST"))
+def show():
+    if request.method == "POST":
+        error=None
+        jsonData = request.get_json()
+        id = jsonData["id"]
+        if not id:
+            error = "Artist ID is required."
+        if error is not None:
+            flash(error)
+        else:
+            cur = (get_db().cursor().execute(
+                "select name,introduction\
+                from artist\
+                where id="+str(id)
+            ))
+            my_query = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
+            return json.dumps(my_query, cls=encoder)
+
 class Artist(object):
     def __init__(self, artist_id, name, introduction):
         self.artist_id = artist_id
--- app/data/model/practice.py
+++ app/data/model/practice.py
@@ -4,6 +4,8 @@ from flask import flash
 from flask import url_for
 from flask import redirect
 from app.data.database.db import get_db
+from app.data.json.encoder import ComplexEncoder as encoder
+import json
 
 bp = Blueprint("practice", __name__)
 
@@ -54,6 +56,15 @@ def delete_practice():
     db.commit()
     return "Delete success!"
 
+@bp.route("/show_practice")
+def show():
+    cur = (get_db().cursor().execute(
+        "select music.id,music.created,music.music_name,music.artist_id,music.difficulty\
+        from music,practice\
+        where music.id=practice.music_id"
+    ))
+    my_query = [dict((cur.description[i][0], value) for i, value in enumerate(row)) for row in cur.fetchall()]
+    return json.dumps(my_query, cls=encoder)
 
 class Practice(object):
     def __init__(self, practice_id, music_id, player_id, score, content):
