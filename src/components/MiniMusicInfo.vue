5126598a80be375e7326a745bc3d994657d70f79
bebinca <bebincaa@163.com>
2020-08-08 13:27:03 +0800

    Modify UserInfo.vue

---
+++ src/components/MiniMusicInfo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-bind:id="'music-' + music.id " style="margin-bottom: 1rem;">
+    <md-list-item to="/test" exact>
+      <div class = "music-text">
+        <span class="md-title" >{{ music.music_name }}</span>
+        <p class = "md-info">Artist ID: {{ music.artist_id }} &amp; Difficulty: {{ music.difficulty }}</p>
+      </div>
+      <code>/</code>
+    </md-list-item>
+    <md-divider></md-divider>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MiniMusicInfo",
+  props: ['index', 'music']
+}
+</script>
+
+<style scoped>
+  .md-title {
+    color: rgba(100,100,100,1);
+  }
+</style>
--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,13 +1,92 @@
+// TOTO: user info with backend
+// avatar.jpg is used for test, should be deleted later
 <template>
-  <p>User info page</p>
+  <div style="position: absolute;">
+    <div class="info">
+      <img class="avatar" :src="imgsrc">
+      <p class = "nickname">nickname</p>
+      <p class = "name">name</p>
+    </div>
+    <div class="music_list">
+      <h2 style = "color: rgba(80,80,80,1);">Music list</h2>
+      <md-divider></md-divider>
+      <div v-if="musics.length" id="music-list-data">
+        <md-list>
+          <music-info v-for="(music, index) in musics"
+                    v-bind:key="music.id"
+                    v-bind:index="index"
+                    v-bind:music="music"
+          ></music-info>
+        </md-list>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import avatar from "@/assets/avatar.jpg";
+import MiniMusicInfo from "@/components/MiniMusicInfo";
+
 export default {
-  name: "UserInfo"
+  name: "UserInfo",
+  components: {
+    'music-info': MiniMusicInfo
+  },
+  data: function() {
+    return {
+      imgsrc:avatar,
+      musics: []
+    }
+  },
+  created: function () {
+    this.getMusics();
+  },
+  methods: {
+    getMusics() {
+      this.$http.get('/show')
+          .then(res => {
+            this.musics = res.data;
+            this.loading = false;
+          })
+          .catch(err => {
+            console.error(err);
+            this.loadingMessage = err;
+          });
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+  .info {
+    margin-top: 2rem; 
+    margin-left: 50px; 
+    width:200px;
+  }
+  .avatar {
+    width:200px; 
+    height:200px; 
+    border-radius: 50%;
+  }
+  .nickname {
+    font-size: 20px; 
+    font-weight: bold; 
+    margin-top: 1rem;
+  }
+  .name {
+    font-size: 15px; 
+    margin-top: -1.1rem; 
+  }
+  .music_list {
+    position: absolute; 
+    left: 320px; top: 0px; right: 0px; 
+    width:1000px;
+  }
+  .md-list {
+    width: 1000px;
+    max-width: 100%;
+    display: inline-block;
+    vertical-align: top;
+    border: 1px solid rgba(#000, .12);
+  }
 </style>
