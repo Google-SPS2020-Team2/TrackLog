96b009f49b26543b62af31d38a6e0c5bb443ece9
Tianyun Zhang <doowzs@qq.com>
2020-08-09 16:29:42 +0800

    add register logic

--- src/components/UserLogin.vue
+++ src/components/UserLogin.vue
@@ -5,11 +5,28 @@
       <label>Username</label>
       <md-input v-model="username"></md-input>
     </md-field>
+    <md-field v-if="isRegister">
+      <label>Nickname</label>
+      <md-input v-model="nickname"></md-input>
+    </md-field>
     <md-field>
       <label>Password</label>
       <md-input v-model="password" type="password"></md-input>
     </md-field>
-    <md-button style="float: right;" v-on:click="login()">Login</md-button>
+    <md-button v-on:click="isRegister = !isRegister">
+      <span v-if="isRegister">Login</span>
+      <span v-else>Register</span>
+    </md-button>
+    <md-button v-if="isRegister"
+               class="md-raised md-primary float-right"
+               v-on:click="register()">
+      Submit
+    </md-button>
+    <md-button v-else
+               class="md-raised md-primary float-right"
+               v-on:click="login()">
+      Login
+    </md-button>
   </div>
 </template>
 
@@ -18,11 +35,26 @@ export default {
   name: "UserLogin",
   data: function () {
     return {
+      isRegister: false,
       username: '',
+      nickname: '',
       password: ''
     }
   },
   methods: {
+    register() {
+      this.$http.post('/register', {
+        username: this.username,
+        nickname: this.nickname,
+        password: this.password
+      })
+          .then(() => {
+            this.login();
+          })
+          .catch(err => {
+            console.error(err);
+          })
+    },
     login() {
       this.$http.post('/login', {
         username: this.username,
@@ -43,5 +75,9 @@ export default {
 <style scoped>
 #userLogin {
   max-width: 300px;
+}
+
+.float-right {
+  float: right;
 }
 </style>
