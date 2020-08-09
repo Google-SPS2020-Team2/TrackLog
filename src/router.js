8830b862e16010f78dd3d1ba4ac72d87cbb558f7
Tianyun Zhang <doowzs@qq.com>
2020-08-09 15:49:59 +0800

    redirect user to login page if the route is protected

--- src/router.js
+++ src/router.js
@@ -7,14 +7,30 @@ import PlayedList from '@/components/PlayedList';
 import UserInfo from '@/components/UserInfo';
 import UserLogin from '@/components/UserLogin.vue';
 
+import store from './store';
+
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   routes: [
     {path: '/', name: 'Home', component: AppHome},
     {path: '/login', name: 'UserLogin', component: UserLogin},
-    {path: '/music', name: 'MusicList', component: MusicList},
-    {path: '/played', name: 'PlayedList', component: PlayedList},
-    {path: '/user', name: 'UserInfo', component: UserInfo}
+    {path: '/music', name: 'MusicList', component: MusicList, meta: {requiresLogin: true}},
+    {path: '/played', name: 'PlayedList', component: PlayedList, meta: {requiresLogin: true}},
+    {path: '/user', name: 'UserInfo', component: UserInfo, meta: {requiresLogin: true}}
   ]
 });
+
+/**
+ * Check whether the route requires user logged in before viewing.
+ * If required and user has not logged in, redirect user to login.
+ */
+router.beforeResolve((to, from, next) => {
+  const isLoggedIn = store.getters.userId !== null;
+  if (to.matched.some(record => record.meta.requiresLogin) && !isLoggedIn) {
+    next("/login");
+  }
+  next();
+});
+
+export default router;
