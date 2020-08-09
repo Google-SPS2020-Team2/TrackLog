79445f1f2f15dab96817ca924b7835038d5c6811
Tianyun Zhang <doowzs@qq.com>
2020-08-09 16:24:14 +0800

    add login logic

--- src/components/UserLogin.vue
+++ src/components/UserLogin.vue
@@ -1,13 +1,47 @@
 <template>
-  <p>Todo: User Login page</p>
+  <div id="userLogin">
+    <h1>Login to TrackLog</h1>
+    <md-field>
+      <label>Username</label>
+      <md-input v-model="username"></md-input>
+    </md-field>
+    <md-field>
+      <label>Password</label>
+      <md-input v-model="password" type="password"></md-input>
+    </md-field>
+    <md-button style="float: right;" v-on:click="login()">Login</md-button>
+  </div>
 </template>
 
 <script>
 export default {
-name: "UserLogin"
+  name: "UserLogin",
+  data: function () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    login() {
+      this.$http.post('/login', {
+        username: this.username,
+        password: this.password
+      })
+          .then(() => {
+            this.$store.commit('userLogin', this.username);
+            this.$router.push({path: '/'});
+          })
+          .catch(err => {
+            console.error(err);
+          })
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+#userLogin {
+  max-width: 300px;
+}
 </style>
